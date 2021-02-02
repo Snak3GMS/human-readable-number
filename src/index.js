02eeb55aa388c9bb56617fc1849a8eee8291ebf7
vova <vova.shkar74@gmail.com>
2021-02-02 17:14:26 +0300

    feat: makes toReadable func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+    const oneToNineteen = [
+            '', 'one','two', 'three', 'four',
+            'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+        ],
+        dozens = ['', '', 'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let    numTensAndOnes=0;
+
+    function makeWord([hundr, tens, ones]){
+        if (tens<2) {
+            numTensAndOnes = tens*10 + (+ones);
+            return [
+                hundr == 0 ? '' :(ones == 0 & tens == 0) ? oneToNineteen[hundr] + ' hundred' : oneToNineteen[hundr] + ' hundred ' ,
+                (ones == 0 && tens == 0 && hundr == 0) ? 'zero' : oneToNineteen[numTensAndOnes]
+            ]
+        }
+        return [
+            hundr == 0 ? '' : oneToNineteen[hundr] + ' hundred ',
+            tens == 0 ? '' : ones>0? dozens[tens] + ' ' : dozens[tens],
+            ones == 0 ? '' : oneToNineteen[ones]
+        ];
+    }    
+    let arr = (number < 100 && number > 9) ? ('0'+number).split('') : (number < 10) ? ('00'+number).split('') :(''+number).split('');
+    return makeWord(arr).join('');
 }
